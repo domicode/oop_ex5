a1638fdca33b00ba624af1dc8e0d55c30b33601c
domicode <domi@domidesign.ch>
2014-03-27 13:40:37 -0400

    added teacher and student

--- people.rb
+++ people.rb
@@ -1,6 +1,13 @@
 class Person 
+
+  attr_accessor :name
+
   def initialize(name)
     @name = name
+  end
+
+  def greeting
+     puts "Hi, my name is #{@name}"
   end
 end
 
@@ -19,3 +26,9 @@ class Instructor < Person
     puts "Everyting in a Ruby is an object"
   end
 end
+
+chris = Instructor.new("Chris")
+chris.greeting
+
+cristina = Student.new("Cristina")
+cristina.greeting
